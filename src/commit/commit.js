bf09c4fe9040807651654d4b450be804d6ee6b3f
Your <you@example.com>
2023-07-28 17:10:06 +0700

    liem commit

---
+++ src/commit/commit.js
@@ -0,0 +1,5 @@
+function Commit() {
+  return <div className="">hello</div>;
+}
+
+export default Commit;
--- src/container/Search.jsx
+++ src/container/Search.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import SearchInput from "../componets/SearchInput";
 import useDebounce from "../hooks/useDoube";
 
+import { Autoplay, EffectFade, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { product } from "../ultils/constant";
+
 function Search() {
   const [searchValue, setSearchValue] = useState("");
 
@@ -13,6 +18,28 @@ function Search() {
       <SearchInput searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="absolute"></div>
       <ListSearch searchTerm={debouncedSearchValue} />
+      <Swiper
+        className="swiper"
+        modules={[Autoplay, EffectFade, Pagination]}
+        slidesPerView={1}
+        // effect='fade'
+        // fadeEffect={{ crossFade: true }}
+        loop={true}
+        scrollbar={{ draggable: true }}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        speed={800}
+      >
+        <div className="">
+          {product?.map((item) => (
+            <SwiperSlide>
+              <img src={item.img} alt="" />
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
     </div>
   );
 }
